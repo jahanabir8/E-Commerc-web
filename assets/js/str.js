e900166551964031687ce56091e9d75b6e7b4861
jahanabir8 <abir.opediatech@gmail.com>
2022-07-24 15:54:48 +0600

    login

--- assets/js/str.js
+++ assets/js/str.js
@@ -1,6 +1,7 @@
 let zoomer = function (){
     document.querySelector('#img-zoomer-box')
       .addEventListener('mousemove', function(e) {
+        
   
       let original = document.querySelector('#img-1'),
           magnified = document.querySelector('#img-2'),
@@ -11,7 +12,7 @@ let zoomer = function (){
           imgHeight = original.offsetHeight,
           xperc = ((x/imgWidth) * 100),
           yperc = ((y/imgHeight) * 100);
-  
+          console.log( original.offsetHeight)
       //lets user scroll past right edge of image
       if(x > (.01 * imgWidth)) {
         xperc += (.15 * xperc);
@@ -25,8 +26,8 @@ let zoomer = function (){
       style.backgroundPositionX = (xperc - 9) + '%';
       style.backgroundPositionY = (yperc - 9) + '%';
   
-      style.left = (x - 180) + 'px';
-      style.top = (y - 180) + 'px';
+      style.left = (x - 200) + 'px';
+      style.top = (y - 200) + 'px';
   
     }, false);
   }();
