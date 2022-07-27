0844a4ac50cac4c940821ed4b05f64590a29c8ae
jahanabir8 <abir.opediatech@gmail.com>
2022-07-27 18:12:23 +0600

    today

--- assets/js/script.js
+++ assets/js/script.js
@@ -227,3 +227,40 @@ $(document).ready(function() {
 });
 
 
+
+// new one
+let zoomer = function (){
+    document.querySelector('#img-zoomer-box')
+      .addEventListener('mousemove', function(e) {
+        
+  
+      let original = document.querySelector('#img-1'),
+          magnified = document.querySelector('#img-2'),
+          style = magnified.style,
+          x = e.pageX - this.offsetLeft,
+          y = e.pageY - this.offsetTop,
+          imgWidth = original.offsetWidth,
+          imgHeight = original.offsetHeight,
+          xperc = ((x/imgWidth) * 100),
+          yperc = ((y/imgHeight) * 100);
+          console.log( original.offsetHeight)
+      //lets user scroll past right edge of image
+      if(x > (.01 * imgWidth)) {
+        xperc += (.15 * xperc);
+      };
+  
+      //lets user scroll past bottom edge of image
+      if(y >= (.01 * imgHeight)) {
+        yperc += (.15 * yperc);
+      };
+  
+      style.backgroundPositionX = (xperc - 9) + '%';
+      style.backgroundPositionY = (yperc - 9) + '%';
+  
+      style.left = (x - 200) + 'px';
+      style.top = (y - 200) + 'px';
+  
+    }, false);
+  }();
+
+
